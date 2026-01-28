e408fb1248f6f2089ed637f0fe05b5806acaa03b
sofyakasurina-lab <sofyakasurina@gmail.com>
2026-01-28 13:03:40 +0700

    Update models.py

--- memes_app/models.py
+++ memes_app/models.py
@@ -1,1 +1,17 @@
 from django.db import models
+
+class Category(models.Model):
+    pass
+
+class Meme(models.Model):
+    pass
+
+class Tag(models.Model):
+    pass
+
+class Favorite(models.Model):
+    pass
+
+class MemeOfTheDay(models.Model):
+    pass
+
