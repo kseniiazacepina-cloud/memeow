c6f448c62a669cddf8f7f2d43210a97724edc8e6
sofyakasurina-lab <sofyakasurina@gmail.com>
2026-01-28 13:12:22 +0700

    Update models.py

--- users_app/models.py
+++ users_app/models.py
@@ -2,4 +2,17 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    pass
+    ZODIAC_SIGNS = [
+        ('aries', 'Овен'),
+        ('taurus', 'Телец'),
+        ('gemini', 'Близнецы'),
+        ('cancer', 'Рак'),
+        ('leo', 'Лев'),
+        ('virgo', 'Дева'),
+        ('libra', 'Весы'),
+        ('scorpio', 'Скорпион'),
+        ('sagittarius', 'Стрелец'),
+        ('capricorn', 'Козерог'),
+        ('aquarius', 'Водолей'),
+        ('pisces', 'Рыбы'),
+    ]
