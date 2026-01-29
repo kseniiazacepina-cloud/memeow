228aedc6298b3ad912e123e986f5d8b8dd096119
sofyakasurina-lab <sofyakasurina@gmail.com>
2026-01-29 12:11:10 +0700

    Update models.py

--- users_app/models.py
+++ users_app/models.py
@@ -3,7 +3,5 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
-    telegram_id = models.CharField(max_length=100, blank=True, null=True)
     email_subscription = models.BooleanField(default=False)
-    telegram_subscription = models.BooleanField(default=False)
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
