a09ba4b444b31384309950008080bd1080c28b35
sofyakasurina-lab <sofyakasurina@gmail.com>
2026-02-06 12:50:17 +0700

    Исправление редактирования.

--- memeow_project/settings.py
+++ memeow_project/settings.py
@@ -73,7 +73,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'memeow',
         'USER': 'postgres',
-        'PASSWORD': 'Postgres',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
     }
