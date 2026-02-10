2ea230c2e1ed8785b19f5778fdffd59340b821e1
sofyakasurina-lab <sofyakasurina@gmail.com>
2026-02-10 19:22:37 +0700

    Update settings.py

--- memeow_project/settings.py
+++ memeow_project/settings.py
@@ -73,7 +73,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'memeow',
         'USER': 'postgres',
-        'PASSWORD': '123',
+        'PASSWORD': '1234',
         'HOST': 'localhost',
         'PORT': '5432',
     }
