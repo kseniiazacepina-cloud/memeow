5abfcbcf88269060de64a2a8183ead360896ee6c
sofyakasurina-lab <sofyakasurina@gmail.com>
2026-02-13 01:34:20 +0700

    Ничего особенного, просто проверить решила работу сайта.

--- memeow_project/settings.py
+++ memeow_project/settings.py
@@ -75,7 +75,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'memeow',
         'USER': 'postgres',
-        'PASSWORD': 'Postgres',
+        'PASSWORD': '1234',
         'HOST': 'localhost',
         'PORT': '5432',
     }
